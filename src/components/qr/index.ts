c6877d1781202f41705834852f355546fa17216a
tien-do <dodinhtn@gmail.com>
2026-01-18 11:39:53 +0700

    chore: Remove QRScanner

--- src/components/qr/index.ts
+++ src/components/qr/index.ts
@@ -1,4 +1,3 @@
 export { QRGenerator } from './QRGenerator';
 export { CodeVerification } from './CodeVerification';
-export { QRScanner } from './QRScanner';
 export { QRScannerInline } from './QRScannerInline';
