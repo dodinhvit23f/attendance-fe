f75056e39fe23f1deed7162d1f4c98645cdc8607
Tom-T.Do <dodinhtn@gmail.com>
2026-05-11 15:32:52 +0700

    feat: Add logo in loading-screen.tsx

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -19,7 +19,7 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
-const PUBLIC_PATHS = ['/', "/auth"];
+const PUBLIC_PATHS = ['/', '/auth', '/subscribe', '/signup'];
 
 const getUserRoles = (): string[] => {
   const rolesStr = localStorage.getItem(STORAGE_KEYS.ROLES);
